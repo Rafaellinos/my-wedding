34ac0ff93c05a0d5b3ce62aa0bafd58eb0c0be53
Rafaellinos <Hadiya@nero123>
2023-01-23 21:05:48 -0300

    fix iframe add title

--- src/routes/footer.component.jsx
+++ src/routes/footer.component.jsx
@@ -33,6 +33,7 @@ const FooterComponent = () => {
                                 <Container>
                                     <footer className={"text-center text-xl-center bg-white footer"}>
                                         <iframe
+                                            title="google-maps"
                                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.59179456155!2d-46.62120694885336!3d-23.475184564227032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef65b3c15c91b%3A0x677d762df0a68a05!2sAv.%20%C3%81gua%20Fria%2C%201608%20-%20Tucuruvi%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2002332-001!5e0!3m2!1sen!2sbr!4v1674128392155!5m2!1sen!2sbr"
                                             style={{border: 0}} allowFullScreen="" loading="lazy"
                                             referrerPolicy="no-referrer-when-downgrade"/>
