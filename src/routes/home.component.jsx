d428bba1950f2eaa2aa3a25fdc9e66c02a60e009
Rafael Lino <rafael.veloso.lino@hotmail.com>
2023-02-13 08:49:18 -0300

    add new pages

--- src/routes/home.component.jsx
+++ src/routes/home.component.jsx
@@ -1,17 +1,25 @@
 import {Container, Image} from "react-bootstrap";
 import './styles.css';
 import MainWeddingPic from '../assets/casamento.png';
+import CountComponent from "../components/count.component";
 
 const Home = () => {
     return (
         <Container>
             <Container>
                 <div className={"text-center my-5"}>
+                    <p className="text-uppercase">
+                        Vamos nos Casar!
+                    </p>
+                    <p>
+                        <strong>21 de Outubro de 2023</strong>
+                    </p>
                     <Image
                         src={MainWeddingPic}
                         alt={"Yasmin and Rafael picture"}
                         className={"img-fluid rounded"}
                     />
+                    <CountComponent/>
                 </div>
             </Container>
         </Container>
